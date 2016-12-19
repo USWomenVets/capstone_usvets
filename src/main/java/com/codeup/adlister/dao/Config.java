5e627016516c8483ecd8b2297bec4b1e9562a70f
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2016-12-19 16:11:14 -0600

    corrected issue not able to resolve import com.mysql.cj.jdbc.Driver; location. had to update imported library

---
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -0,0 +1,30 @@
+package com.codeup.adlister.dao;
+
+/**
+ * Created by KJCurtis on 12/15/16.
+ */
+
+
+
+
+public class Config {
+    private String url = "jdbc:mysql://localhost/mysql";
+    private String user = "root";
+    private String password = "codeup";
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+
+}
